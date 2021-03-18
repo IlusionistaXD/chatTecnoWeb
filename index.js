bd250339e0f4be7b748f09e1cdeb9bf8fcde8c57
PabloXD <ilusionistaxd@gmail.com>
2021-03-18 03:17:28 -0400

    Nuevas modificaciones

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 const path = require('path');
 const express = require('express');
-
 const app = express();
-
 //settings
 app.set('port', process.env.PORT || 80);
-
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
-
 //iniciando servidor
 const server = app.listen(app.get('port'), () => {
     console.log('Servidor en puerto:', app.get('port'));
 });
-
 // web scokets
 const SocketIO = require('socket.io');
 const io = SocketIO(server);
-
 io.on('connection', (socket) => {
     console.log('New conection', socket.id);
-
     socket.on('chat:message', (data) => {
         console.log(data);
         io.sockets.emit('chat:message', data);
     });
-
     socket.on('chat:typing', (data) => {
         socket.broadcast.emit('chat:typing', data);
-
     })
 });
 
--- public/chat.js
+++ public/chat.js
@@ -1,12 +1,10 @@
 const socket = io();
-
 //dom elementee
 let message = document.getElementById('message');
 let username = document.getElementById('username');
 let btn = document.getElementById('send');
 let output = document.getElementById('output');
 let actions = document.getElementById('actions');
-
 btn.addEventListener('click', function  () {
     socket.emit('chat:message', {
         message: message.value,
@@ -14,23 +12,19 @@ btn.addEventListener('click', function  () {
     });
     console.log(username.value, message.value);
 });
-
 message.addEventListener('keypress', function () {
     socket.emit('chat:typing', username.value);
 });
-
 socket.on('chat:message', function (data) {
-//limpia la accion
-actions.innerHTML = '';
-
-output.innerHTML += `<p>
-    <strong>${data.username}</strong>: ${data.message}
-<p>`
+    //limpia la accion
+    actions.innerHTML = '';
+    output.innerHTML += `<p>
+        <strong>${data.username}</strong>: ${data.message}
+    <p>`
 });
-
 socket.on('chat:typing', function (data) {
     actions.innerHTML = `<p>
-    <em>${data} is typing a message </em>
+    <em>${data} esta escribiendo un mensaje </em>
     </p>`
 });
 
